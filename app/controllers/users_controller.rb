450f9f5fef335fd726507e010de39fd1534f4fbb
Sophie Gu <sophieqgu@gmail.com>
2020-12-31 20:27:52 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :verify_user, unless: [:new, :create]
+  before_action :verify_user, only: [:show, :edit, :update, :destroy]
 
   def new
     @user = User.new
