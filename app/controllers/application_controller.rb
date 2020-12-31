816117a645267556b250b2c6424464a1a817a503
Sophie Gu <sophieqgu@gmail.com>
2020-12-31 20:02:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :verify_user, :current_user, :logged_in, :admin 
+  helper_method :verify_user, :current_user, :logged_in, :admin
   before_action :verify_user
 
   def home
+  end
+
+  def verify_user
+    redirect_to '/' unless logged_in
+  end
+
+  def logged_in
+    !!session[:user_id]
+  end
+
+  def current_user
+    @user ||= User.find_by(id: session[:user_id])
+  end
+
+  def admin
+    current_user.admin
   end
 
 end
