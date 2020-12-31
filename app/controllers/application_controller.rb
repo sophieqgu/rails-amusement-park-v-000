3f0c546e8682c3bb01f68b8ce1ff55164e1b45a5
Sophie Gu <sophieqgu@gmail.com>
2020-12-31 20:19:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :verify_user, :current_user, :logged_in, :admin
-  before_action :verify_user
+  
 
   def home
   end
