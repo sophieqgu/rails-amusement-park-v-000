d89ba66a50b91a6382c51ec7f8ad1baa0faca88a
Sophie Gu <sophieqgu@gmail.com>
2020-12-31 20:45:40 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,8 +2,8 @@ class RidesController < ApplicationController
 
   def create
     ride = Ride.create(ride_params)
-    message = ride.take_ride
-    redirect_to user_path(ride.user), flash: { message: message }
+
+    redirect_to user_path(ride.user), notice: ride.take_ride
   end
 
   private
