18001132744df06893386f1fc27966e91cd6d694
Sophie Gu <sophieqgu@gmail.com>
2020-12-31 20:19:44 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_user, only: [:new, :create]
+  before_action :verify_user, only: [:new, :create]
 
   def new
     @user = User.new
