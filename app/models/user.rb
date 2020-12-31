2381ba5df2655b0caf232f3395ca2baa0855d18a
Sophie Gu <sophieqgu@gmail.com>
2020-12-31 19:54:01 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
 
+  def mood
+    happiness > nausea ? "happy" : "sad"
+  end
 
 end
