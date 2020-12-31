4ba37b59a562d770f44d7d13c7454628daa0925f
Sophie Gu <sophieqgu@gmail.com>
2020-12-31 20:56:31 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -3,7 +3,11 @@ class RidesController < ApplicationController
   def create
     @ride = Ride.create(ride_params)
 
-    redirect_to user_path(@ride.user), notice: @ride.take_ride
+    if @ride.valid?
+      redirect_to user_path(current_user), notice:@ride.take_ride
+    else
+      redirect_to attraction_path(@attraction)
+    end 
   end
 
   private
