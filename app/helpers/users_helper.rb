bac6598b7db2c8273015ed9af454001ae75fceee
Sophie Gu <sophieqgu@gmail.com>
2020-12-31 20:02:03 +0000

    Automatically backed up by Learn

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,19 +1,5 @@
 module UsersHelper
 
-  def verify_user
-    redirect_to '/' unless logged_in
-  end
-
-  def logged_in
-    !!session[:user_id]
-  end
-
-  def current_user
-    @user ||= User.find_by(id: session[:user_id])
-  end
-
-  def admin
-    current_user.admin
-  end
+  
 
 end
